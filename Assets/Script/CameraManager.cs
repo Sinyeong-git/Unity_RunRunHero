e0a0bc4070c076f1ff96ea7c7f83747c9116dad2
Sinyeong-git <ksa9408@gmail.com>
2020-02-02 02:55:30 +0900

    2020-02-02

---
+++ Assets/Script/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject Player;
+    Transform PT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        PT = Player.transform;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 PlayerPosition_temp = PT.position();
+        transform.position = Vector3.Lerp(transform.position, PT.position, 2f * Time.deltaTime);
+        transform.Translate(0, 0, -1);     
+    }
+}
